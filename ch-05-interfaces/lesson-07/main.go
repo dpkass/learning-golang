e78953475513a8e311d47cbba1b35c63208c9cb2
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2025-09-07 04:40:14 +0200

    chapter 5, lesson 7: solution

--- ch-05-interfaces/lesson-07/main.go
+++ ch-05-interfaces/lesson-07/main.go
@@ -1,11 +1,23 @@
 package main
 
+import "fmt"
+
 func (e email) cost() int {
-	// ?
+	if e.isSubscribed {
+		return 2 * len(e.body)
+	} else {
+		return 5 * len(e.body)
+	}
 }
 
 func (e email) format() string {
-	// ?
+	var subscribedText string
+	if e.isSubscribed {
+		subscribedText = "Subscribed"
+	} else {
+		subscribedText = "Not Subscribed"
+	}
+	return fmt.Sprintf("'%s' | %s", e.body, subscribedText)
 }
 
 type expense interface {
