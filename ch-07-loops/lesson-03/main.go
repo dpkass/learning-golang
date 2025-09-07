785c886eccf8f9b3d48b8122a322d0ed5b0662e2
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2025-09-07 22:54:52 +0200

    chapter 7, lesson 03: solution
    incl. code simplification

--- ch-07-loops/lesson-03/main.go
+++ ch-07-loops/lesson-03/main.go
@@ -2,15 +2,12 @@ package main
 
 func getMaxMessagesToSend(costMultiplier float64, maxCostInPennies int) int {
 	actualCostInPennies := 1.0
-	maxMessagesToSend := 1
-	balance := float64(maxCostInPennies) - actualCostInPennies
-	for {
-		actualCostInPennies *= costMultiplier
+	maxMessagesToSend := 0
+	balance := float64(maxCostInPennies)
+	for balance >= actualCostInPennies {
 		balance -= actualCostInPennies
 		maxMessagesToSend++
-	}
-	if balance < 0 {
-		maxMessagesToSend--
+		actualCostInPennies *= costMultiplier
 	}
 	return maxMessagesToSend
 }
