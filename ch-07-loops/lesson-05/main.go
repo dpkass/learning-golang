04e4e55f7f49784c0458ccb247dc490a27c22d10
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2025-09-07 23:24:09 +0200

    chapter 7, lesson 05: solution

--- ch-07-loops/lesson-05/main.go
+++ ch-07-loops/lesson-05/main.go
@@ -4,8 +4,22 @@ import (
 	"fmt"
 )
 
+// the goal of the exercise is using break/continue thats why
+// no need to puke, when looking at the code
 func printPrimes(max int) {
-	// ?
+	for n := 2; n <= max; n++ {
+		isPrime := true
+		for i := 2; i*i <= n; i++ {
+			if n%i == 0 {
+				isPrime = false
+				break
+				// or continue outer, but we didn't do labels yet
+			}
+		}
+		if isPrime {
+			fmt.Println(n)
+		}
+	}
 }
 
 // don't edit below this line
