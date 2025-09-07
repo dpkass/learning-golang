36f022b1e829ba243a13470b0660efd09c91c9c9
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2025-09-07 05:43:47 +0200

    chapter 5, lesson 17: solution

--- ch-05-interfaces/lesson-17/main.go
+++ ch-05-interfaces/lesson-17/main.go
@@ -22,8 +22,31 @@ type systemAlert struct {
 	messageContent string
 }
 
-// ?
+func (d directMessage) importance() int {
+	if d.isUrgent {
+		return 50
+	}
+	return d.priorityLevel
+}
+
+func (g groupMessage) importance() int {
+	return g.priorityLevel
+}
+
+func (s systemAlert) importance() int {
+	return 100
+}
 
 func processNotification(n notification) (string, int) {
-	// ?
+	var msg string
+	switch v := n.(type) {
+	case directMessage:
+		msg = v.senderUsername
+	case groupMessage:
+		msg = v.groupName
+	case systemAlert:
+		msg = v.alertCode
+	}
+
+	return msg, n.importance()
 }
