5738907f8a123ccae62e5242395af5ba5ed8c75c
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2025-09-08 01:08:35 +0200

    chapter 8, lesson 2: solution

--- ch-08-slices/lesson-02/main.go
+++ ch-08-slices/lesson-02/main.go
@@ -10,5 +10,12 @@ const (
 )
 
 func getMessageWithRetriesForPlan(plan string, messages [3]string) ([]string, error) {
-	// ?
+	switch plan {
+	case planPro:
+		return messages[:], nil
+	case planFree:
+		return messages[:2], nil
+	default:
+		return nil, errors.New("unsupported plan")
+	}
 }
