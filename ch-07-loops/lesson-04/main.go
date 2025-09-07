5daf07db1add1d1daf8516edfc16dace4966f754
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2025-09-07 23:10:24 +0200

    chapter 7, lesson 04: solution

--- ch-07-loops/lesson-04/main.go
+++ ch-07-loops/lesson-04/main.go
@@ -1,7 +1,23 @@
 package main
 
+import "fmt"
+
 func fizzbuzz() {
-	// ?
+	for i := 1; i <= 100; i++ {
+		out := ""
+		if i%3 == 0 {
+			out += "Fizz"
+		}
+		if i%5 == 0 {
+			out += "Buzz"
+		}
+
+		if out == "" {
+			fmt.Println(i)
+		} else {
+			fmt.Println(out)
+		}
+	}
 }
 
 // don't touch below this line
