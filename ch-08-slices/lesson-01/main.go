c49e40a09e22683a7a1cf85b1fd67fd1716c6127
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2025-09-08 01:01:14 +0200

    chapter 8, lesson 1: solution

--- ch-08-slices/lesson-01/main.go
+++ ch-08-slices/lesson-01/main.go
@@ -1,5 +1,9 @@
 package main
 
-func getMessageWithRetries(primary, secondary, tertiary string) ([3]string, [3]int) {
-	// ?
+func getMessageWithRetries(primary, secondary, tertiary string) (messages [3]string, costs [3]int) {
+	messages = [3]string{primary, secondary, tertiary}
+	costs[0] = len(messages[0])
+	costs[1] = costs[0] + len(messages[1])
+	costs[2] = costs[1] + len(messages[2])
+	return
 }
