b2cb1c2df1ba054b5636d28bd291ea44258e8255
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2025-09-07 23:29:41 +0200

    chapter 7, lesson 06: solution

--- ch-07-loops/lesson-06/main.go
+++ ch-07-loops/lesson-06/main.go
@@ -1,5 +1,8 @@
 package main
 
-func countConnections(groupSize int) int {
-	// ?
+func countConnections(groupSize int) (count int) {
+	for i := 2; i <= groupSize; i++ {
+		count += i - 1
+	}
+	return
 }
