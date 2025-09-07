6af26525f1a43106fbf4483e48a89c44330d6e15
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2025-09-07 03:44:56 +0200

    chapter 4, lesson 10: solution

--- ch-04-structs/lesson-10/main.go
+++ ch-04-structs/lesson-10/main.go
@@ -1,9 +1,29 @@
 package main
 
+type Membership struct {
+	Type             string
+	MessageCharLimit int
+}
+
 type User struct {
+	Membership
 	Name string
 }
 
 func newUser(name string, membershipType string) User {
-	// ?
+	var messageCharLimit int
+	switch membershipType {
+	case "premium":
+		messageCharLimit = 1000
+	default:
+		messageCharLimit = 100
+	}
+
+	return User{
+		Name: name,
+		Membership: Membership{
+			Type:             membershipType,
+			MessageCharLimit: messageCharLimit,
+		},
+	}
 }
