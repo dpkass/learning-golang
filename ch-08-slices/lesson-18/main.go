33b6b2c4ef453bf9d6681da03c128c5abd2e7edb
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2025-09-08 10:30:00 +0200

    chapter 8, lesson 18: solution

--- ch-08-slices/lesson-18/main.go
+++ ch-08-slices/lesson-18/main.go
@@ -1,5 +1,18 @@
 package main
 
 func isValidPassword(password string) bool {
-	// ?
+	if len(password) < 5 || len(password) > 12 {
+		return false
+	}
+
+	hasUpper, hasDigit := false, false
+	for _, c := range password {
+		if 'A' <= c && c <= 'Z' {
+			hasUpper = true
+		}
+		if '0' <= c && c <= '9' {
+			hasDigit = true
+		}
+	}
+	return hasUpper && hasDigit
 }
