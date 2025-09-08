586ff59860997b863e6c59e1867c3b032af0ae09
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2025-09-08 09:39:31 +0200

    chapter 8, lesson 11: solution

--- ch-08-slices/lesson-11/main.go
+++ ch-08-slices/lesson-11/main.go
@@ -6,5 +6,12 @@ type cost struct {
 }
 
 func getDayCosts(costs []cost, day int) []float64 {
-	// ?
+	dayCosts := []float64{}
+	for i := 0; i < len(costs); i++ {
+		if costs[i].day != day {
+			continue
+		}
+		dayCosts = append(dayCosts, costs[i].value)
+	}
+	return dayCosts
 }
