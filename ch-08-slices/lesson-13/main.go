b0d6428079d1475ec0b43a846b5061c6d37363f6
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2025-09-08 10:00:51 +0200

    chapter 8, lesson 13: solution

--- ch-08-slices/lesson-13/main.go
+++ ch-08-slices/lesson-13/main.go
@@ -1,5 +1,12 @@
 package main
 
 func createMatrix(rows, cols int) [][]int {
-	// ?
+	mat := make([][]int, rows)
+	for i := range rows {
+		mat[i] = make([]int, cols)
+		for j := range cols {
+			mat[i][j] = i * j
+		}
+	}
+	return mat
 }
