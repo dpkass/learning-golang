b1ae4eb921215b949d56e13114347a2356557150
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2025-09-08 10:50:06 +0200

    chapter 8, lesson 19: solution

--- ch-08-slices/lesson-19/main.go
+++ ch-08-slices/lesson-19/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "strings"
+
 type sms struct {
 	id      string
 	content string
@@ -7,10 +9,25 @@ type sms struct {
 }
 
 func tagMessages(messages []sms, tagger func(sms) []string) []sms {
-	// ?
+	for i := 0; i < len(messages); i++ {
+		tags := tagger(messages[i])
+		if tags == nil {
+			tags = []string{}
+		}
+		messages[i].tags = tags
+	}
+	return messages
 }
 
 func tagger(msg sms) []string {
 	tags := []string{}
-	// ?
+	lower := strings.ToLower(msg.content)
+	if strings.Contains(lower, "urgent") {
+		tags = append(tags, "Urgent")
+	}
+
+	if strings.Contains(lower, "sale") {
+		tags = append(tags, "Promo")
+	}
+	return tags
 }
