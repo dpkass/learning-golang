2abf95781b5bcc6d143d550ac73d201a596a6894
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2025-09-13 23:41:28 +0200

    chapter 9, lesson 11: solution

--- ch-09-maps/lesson-11/main.go
+++ ch-09-maps/lesson-11/main.go
@@ -1,5 +1,21 @@
 package main
 
+import "strings"
+
 func countDistinctWords(messages []string) int {
-	// ?
+	words := make(map[string]struct{})
+
+	for _, message := range messages {
+		if message == "" {
+			continue
+		}
+
+		ws := strings.Split(message, " ")
+		for _, w := range ws {
+			w = strings.ToLower(w)
+			words[w] = struct{}{}
+		}
+	}
+
+	return len(words)
 }
