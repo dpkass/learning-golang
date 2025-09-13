d080ae919e54b7fc84c7020f6c187b4f4993e9ed
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2025-09-13 20:25:31 +0200

    chapter 9, lesson 01: solution

--- ch-09-maps/lesson-01/main.go
+++ ch-09-maps/lesson-01/main.go
@@ -3,7 +3,20 @@ package main
 import "errors"
 
 func getUserMap(names []string, phoneNumbers []int) (map[string]user, error) {
-	// ?
+	if len(names) != len(phoneNumbers) {
+		return nil, errors.New("invalid sizes")
+	}
+
+	users := make(map[string]user)
+	for i := range names {
+		new := user{
+			name:        names[i],
+			phoneNumber: phoneNumbers[i],
+		}
+		users[names[i]] = new
+	}
+
+	return users, nil
 }
 
 type user struct {
