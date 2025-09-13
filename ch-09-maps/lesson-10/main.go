ad0f96b4c15974a752bfd9b1e7dbc610cb0704b0
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2025-09-13 23:26:19 +0200

    chapter 9, lesson 10: solution

--- ch-09-maps/lesson-10/main.go
+++ ch-09-maps/lesson-10/main.go
@@ -1,5 +1,15 @@
 package main
 
 func getNameCounts(names []string) map[rune]map[string]int {
-	// Your code here
+	counts := make(map[rune]map[string]int)
+
+	for _, name := range names {
+		runes := []rune(name)
+		if _, ok := counts[runes[0]]; !ok {
+			counts[runes[0]] = make(map[string]int)
+		}
+		counts[runes[0]][name]++
+	}
+
+	return counts
 }
